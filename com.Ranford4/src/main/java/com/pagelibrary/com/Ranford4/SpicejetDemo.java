d9740c5e49479d2df154af71b2b738f5c7b30219
chshivanand1992 <chshivanand1992@gmail.com>
2018-05-24 20:15:33 +0530

    added new project

---
+++ com.Ranford4/src/main/java/com/pagelibrary/com/Ranford4/SpicejetDemo.java
@@ -0,0 +1,30 @@
+package com.pagelibrary.com.Ranford4;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class SpicejetDemo
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
